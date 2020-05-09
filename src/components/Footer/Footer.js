7777290a513924b68033bb3cb519954a90998158
Surenkhuu Shagdarsuren <surenkhuu@gmail.com>
2020-05-08 22:48:05 -0700

    Text Updated

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -5,7 +5,7 @@ function Footer() {
     return (
       <footer className="footer">
         <div className="bottom">
-          This is Footer Component.
+          Thank you for playing the game!
         </div>
       </footer>
     );
